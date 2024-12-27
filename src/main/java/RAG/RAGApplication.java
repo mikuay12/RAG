51c3ab7d7fabdd0d44d274404514935193dee1ab
mikuay12 <mikuay@qq.com>
2024-12-27 14:51:10 +0800

    first commit

---
+++ src/main/java/RAG/RAGApplication.java
@@ -0,0 +1,11 @@
+package RAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RAGApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RAGApplication.class, args);
+    }
+}
